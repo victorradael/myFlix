efdbaedd8ab95be0d9bf274a1bd99c650a54303a
Victor Radael <victorradael@hotmail.com>
2020-07-27 16:31:19 -0300

    :rocket: Day 1 TypeScript: on

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,13 +6,11 @@ import ButtonLink from "../Button";
 
 const Menu: React.FC = () => {
   return (
-    <nav>
+    <nav className="Menu">
       <a href="/">
         <img className="Logo" src={logo} alt="myFlix Logo" />
       </a>
-      <ButtonLink className="ButtonLink" href="/">
-        New video
-      </ButtonLink>
+      <ButtonLink href="/">New video</ButtonLink>
     </nav>
   );
 };
