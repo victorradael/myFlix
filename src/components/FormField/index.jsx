89832898026c65b8c9db3ad4c048279a0828c169
Victor Radael <victorradael@hotmail.com>
2020-08-03 22:40:00 -0300

    :rocket: server with JSON server on Heroku

--- src/components/FormField/index.jsx
+++ src/components/FormField/index.jsx
@@ -1,18 +1,95 @@
-import React from "react";
+import React from 'react';
 
-const FormField = ({ name, value, onChange, type, title }) => (
-  <div>
-    <label>
-      {title}
-      <input
-        name={name}
-        type={type}
-        value={value}
-        name="name"
-        onChange={onChange}
-      />
-    </label>
-  </div>
-);
+import styled, { css } from 'styled-components';
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type='color'] {
+    padding-left: 56px;
+  }
+`;
+
+const Label = styled.label``;
+
+const LabelText = styled.span`
+  color: #e5e5e5;
+  height: 57px;
+  position: absolute;
+  top: 0;
+  left: 16px;
+
+  display: flex;
+  align-items: center;
+
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+
+  transition: 0.1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585d;
+  color: #f5f5f5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585d;
+
+  padding: 16px 16px;
+  margin-bottom: 45px;
+
+  resize: none;
+  border-radius: 4px;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  &:focus:not([type='color']) + ${LabelText} {
+    transform: scale(0.6) translateY(-10px);
+  }
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return (
+      hasValue &&
+      css`
+        &:not([type='color']) + ${LabelText} {
+          transform: scale(0.6) translateY(-10px);
+        }
+      `
+    );
+  }}
+`;
+
+const FormField = ({ name, value, onChange, type, title }) => {
+  const fieldId = `id_${name}`;
+  const isTextarea = type;
+  const tag = isTextarea === 'textarea' ? 'textarea' : 'input';
+  return (
+    <FormFieldWrapper>
+      <Label htmlFor={fieldId}>
+        <Input
+          as={tag}
+          id={fieldId}
+          name={name}
+          type={type}
+          value={value}
+          name="name"
+          onChange={onChange}
+        />
+        <LabelText>{title}:</LabelText>
+      </Label>
+    </FormFieldWrapper>
+  );
+};
 
 export default FormField;
--- src/pages/cadastro/Categoria/index.jsx
+++ src/pages/cadastro/Categoria/index.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import PageDefault from "../../../components/PageDefault";
-import FormField from "../../../components/FormField";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
-  const iFormData = {
-    name: "",
-    description: "",
-    color: "#000",
-  };
+  // const iFormData = {
+  //   name: '',
+  //   description: '',
+  //   color: '',
+  // };
 
   const [categories, setCategories] = useState([]);
-  const [values, setValues] = useState(iFormData);
+  const [values, setValues] = useState();
 
   function setValue(key, value) {
     setValues({
@@ -22,8 +23,22 @@ function CadastroCategoria() {
 
   function handleChange(event) {
     // const { getAttribute, value } = event.target;
-    setValue(event.target.getAttribute("name"), event.target.value);
+    setValue(event.target.getAttribute('name'), event.target.value);
   }
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL).then(async respostaDoServer => {
+        if (respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategories([...resposta]);
+          return;
+        }
+        throw new Error('Não foi possível pegar os dados');
+      });
+    }
+  }, []);
 
   return (
     <PageDefault>
@@ -33,35 +48,37 @@ function CadastroCategoria() {
         onSubmit={function handleSubmit(event) {
           event.preventDefault();
           setCategories([...categories, values]);
-          setValues(iFormData);
+          setValues(categories);
         }}
       >
         <FormField
-          title="Category Name:"
+          title="Category Name"
           name="name"
           type="text"
           value={values.name}
-          onChange={handleChange}
+          onChange={event => handleChange(event)}
         />
 
         <FormField
-          title="Description:"
+          title="Description"
           name="description"
-          type="text"
+          type="textarea"
           value={values.description}
-          onChange={handleChange}
+          onChange={event => handleChange(event)}
         />
 
         <FormField
-          title="Color:"
+          title="Color"
           name="color"
           type="color"
           value={values.color}
-          onChange={handleChange}
+          onChange={event => handleChange(event)}
         />
 
         <button>Cadastrar</button>
       </form>
+
+      {categories.length === 0 && <div>Loading ...</div>}
 
       <ul>
         {categories.map((category, index) => (
